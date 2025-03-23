4e7512b0d82653fb689c4c26e6e6eb2623bc2836
krishanvk17 <krishan.vallamkon.7@gmail.com>
2025-03-23 11:23:18 -0500

    Update chooser.py

--- chooser.py
+++ chooser.py
@@ -6,6 +6,6 @@ num=st.slider("Choose how many options you have.",1,10)
 choice=random.randint(1,num)
 
 st.write("Think or write out which option each number is.")
-if st.button:
+if st.button("Choose..."):
   st.write("I choose...",choice,"!")
   st.info("Refresh your screen to try again!")
